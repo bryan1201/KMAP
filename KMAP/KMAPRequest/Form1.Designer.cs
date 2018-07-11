a53db8d674f6aeafb99e1c65bb7df29e4ed02098
Bryan Wu <Wu.Bryan@inventec.com>
2018-07-11 16:44:27 +0800

    UPdataKM.KMAPRequest

---
+++ KMAP/KMAPRequest/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace KMAPRequest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAdvSearch = new System.Windows.Forms.Button();
+            this.txtAdvFolderID = new System.Windows.Forms.TextBox();
+            this.JsonResultTxt = new System.Windows.Forms.TextBox();
+            this.txtAdvKeyword = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnAdvSearch
+            // 
+            this.btnAdvSearch.Location = new System.Drawing.Point(24, 24);
+            this.btnAdvSearch.Name = "btnAdvSearch";
+            this.btnAdvSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnAdvSearch.TabIndex = 0;
+            this.btnAdvSearch.Text = "AdvSearch";
+            this.btnAdvSearch.UseVisualStyleBackColor = true;
+            this.btnAdvSearch.Click += new System.EventHandler(this.btnAdvSearch_Click);
+            // 
+            // txtAdvFolderID
+            // 
+            this.txtAdvFolderID.Location = new System.Drawing.Point(172, 24);
+            this.txtAdvFolderID.Name = "txtAdvFolderID";
+            this.txtAdvFolderID.Size = new System.Drawing.Size(186, 29);
+            this.txtAdvFolderID.TabIndex = 1;
+            // 
+            // JsonResultTxt
+            // 
+            this.JsonResultTxt.Location = new System.Drawing.Point(12, 188);
+            this.JsonResultTxt.Multiline = true;
+            this.JsonResultTxt.Name = "JsonResultTxt";
+            this.JsonResultTxt.Size = new System.Drawing.Size(570, 285);
+            this.JsonResultTxt.TabIndex = 2;
+            // 
+            // txtAdvKeyword
+            // 
+            this.txtAdvKeyword.Location = new System.Drawing.Point(172, 71);
+            this.txtAdvKeyword.Name = "txtAdvKeyword";
+            this.txtAdvKeyword.Size = new System.Drawing.Size(186, 29);
+            this.txtAdvKeyword.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(594, 485);
+            this.Controls.Add(this.txtAdvKeyword);
+            this.Controls.Add(this.JsonResultTxt);
+            this.Controls.Add(this.txtAdvFolderID);
+            this.Controls.Add(this.btnAdvSearch);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAdvSearch;
+        private System.Windows.Forms.TextBox txtAdvFolderID;
+        private System.Windows.Forms.TextBox JsonResultTxt;
+        private System.Windows.Forms.TextBox txtAdvKeyword;
+    }
+}
+
---
+++ KMAP/KMAPRequest/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Jayrock.Json;
+using System.Net;
+using Jayrock.Json.Conversion;
+using System.IO;
+using System.Web;
+using System.Collections;
+using System.Collections.Specialized;
+
+namespace KMAPRequest
+{
+    public partial class Form1 : Form
+    {
+        private string DataFormat = "JSON";                             // (必須)API資料傳輸格式，建議預設使用JSON
+        private string userid = "admin";                                // (必須)KM系統中有權限讀寫的帳號，建議使用系統管理者帳號
+        private string tenant = "default";
+        private string KMUrl = "http://km.inventec.com/ESP/api/";       // (必須)KM Server Site的API虛擬目錄URL路徑
+        private string GlobalCurrentDocumentId = "";                    // 暫時無作用,不需要去動它
+        private string GlobalCurrentCategoryId = "1";                   // 暫時無作用,不需要去動它
+        private string GlobalSearchKeyword = "KM";
+        private string API_Key = "1722e63fda0740e4aff711836d9ca0bc";    // (必須)KM系統中已註冊並啟用的API Key
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdvSearch_Click(object sender, EventArgs e)
+        {
+            this.JsonResultTxt.Text = string.Empty;
+
+            WebClient client2 = new WebClient();
+            client2.Encoding = Encoding.UTF8;
+            string targetAdvSearchUrl = GetServiceUrl(ServiceType.GetAdvancedResult);
+
+            ArrayList al = new ArrayList();
+            NameValueCollection nvc = new NameValueCollection();
+
+            nvc.Add("enabletagsynonyms", "false");
+            nvc.Add("enablekeywordsynonyms", "false");
+            nvc.Add("containchildcategory", "false");
+            nvc.Add("containchildfolder", "false");
+            nvc.Add("keyword", this.txtAdvKeyword.Text.Trim());
+
+            if (this.txtAdvFolderID.Text.Trim() != string.Empty)
+            {
+                nvc.Add("folder", this.txtAdvFolderID.Text.Trim());
+            }
+
+            string uploadData = "";
+            foreach (string k in nvc.Keys)
+            {
+                al.Add(k + "=" + nvc[k]);
+            }
+            uploadData = string.Join("&", (string[])al.ToArray(typeof(string)));
+
+
+            byte[] bytesAdvSearch = Encoding.UTF8.GetBytes(uploadData);
+            byte[] bytesAdvSearchResult = null;
+            string stringAdvSearchResult = "";
+
+            try
+            {
+                client2.Headers.Add("Content-Type", "application/x-www-form-urlencoded");
+                bytesAdvSearchResult = client2.UploadData(targetAdvSearchUrl, bytesAdvSearch);
+                stringAdvSearchResult = Encoding.UTF8.GetString(bytesAdvSearchResult);
+            }
+            catch (Exception ex)
+            {
+                string err = "GetAdvancedResult " + ex.ToString();
+                return;
+            }
+            finally
+            {
+                this.JsonResultTxt.Text = stringAdvSearchResult;
+            }
+        }
+
+        private string GetServiceUrl(ServiceType serviceType)
+        {
+            string ServiceUrl = "";
+
+            switch (serviceType)
+            {
+                case ServiceType.GetRootFolder:
+                    ServiceUrl = GetKmUrl() + "folder/root/public?shell=true&tenant=" + tenant + "&";
+                    break;
+                case ServiceType.UploadFile:
+                    ServiceUrl = GetKmUrl() + "upload?shell=true&tenant=" + tenant + "&";
+                    break;
+                case ServiceType.AcquireDocumentDraft:
+                    ServiceUrl = GetKmUrl() + "document/acquirenewdocumentdraft?shell=true&tenant=" + tenant + "&";
+                    break;
+                case ServiceType.SubmitNewDocument:
+                    ServiceUrl = GetKmUrl() + "document/new?shell=true&tenant=" + tenant + "&";
+                    break;
+                case ServiceType.AllDocumentClass:
+                    ServiceUrl = GetKmUrl() + "documentclass/all/enabled?shell=true&tenant=" + tenant + "&";
+                    break;
+                case ServiceType.GetCategoryInfo:
+                    ServiceUrl = GetKmUrl() + "category/" + GlobalCurrentCategoryId + "?load_path=False&shell=true&tenant=" + tenant + "&";
+                    break;
+                case ServiceType.GetSearchExBySimple:
+                    ServiceUrl = GetKmUrl() + "search/ext/simple/" + GlobalSearchKeyword + "?shell=true&tenant=" + tenant + "&";
+                    break;
+                case ServiceType.GetAdvancedResult:
+                    ServiceUrl = GetKmUrl() + "search/advancedresult" + "?shell=true&tid=0&api_key=" + API_Key + "&who=" + userid + "&format=" + DataFormat + "&tenant=" + tenant + "&";
+                    break;
+                case ServiceType.GetDocument:
+                    ServiceUrl = GetKmUrl() + "document/" + GlobalCurrentDocumentId.ToString() + "?shell=true&tenant=" + tenant + "&";
+                    break;
+                case ServiceType.AddUser:
+                    ServiceUrl = GetKmUrl() + "user/add" + "?shell=true&tenant=" + tenant + "&";
+                    break;
+                case ServiceType.GetUser:
+                    ServiceUrl = GetKmUrl() + "user/{0}" + "?shell=true&tenant=" + tenant + "&";
+                    break;
+                case ServiceType.GetUserBySubjectId:
+                    ServiceUrl = GetKmUrl() + "user/exact/{0}" + "?shell=true&tenant=" + tenant + "&";
+                    break;
+                case ServiceType.UpdateUser:
+                    ServiceUrl = GetKmUrl() + "user/update" + "?shell=true&tenant=" + tenant + "&";
+                    break;
+                case ServiceType.DeleteUser:
+                    ServiceUrl = GetKmUrl() + "user/delete/{0}" + "?shell=true&tenant=" + tenant + "&";
+                    break;
+                case ServiceType.GetAllUser:
+                    ServiceUrl = GetKmUrl() + "user/all" + "?shell=true&tenant=" + tenant + "&";
+                    break;
+                case ServiceType.DownloadFile2:
+                    ServiceUrl = GetKmUrl() + "download2/{0}" + "?shell=true&tenant=" + tenant + "&";
+                    break;
+                case ServiceType.AcquireFolderDraft:
+                    ServiceUrl = GetKmUrl() + "folder/newdraft/{0}" + "?shell=true&tid=0&pi=0&ps=10&api_key=" + API_Key + "&who=" + userid + "&format=" + DataFormat + "&tenant=" + tenant + "&";
+                    break;
+                case ServiceType.AddFolder:
+                    ServiceUrl = GetKmUrl() + "folder/new/{0}" + "?shell=true&tid=0&api_key=" + API_Key + "&who=" + userid + "&format=" + DataFormat + "&tenant=" + tenant + "&";
+                    break;
+                default:
+                    break;
+            }
+            return ServiceUrl;
+        }
+
+        private string GetKmUrl()
+        {
+            return KMUrl;
+        }
+    }
+}
